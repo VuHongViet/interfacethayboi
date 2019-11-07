3023ee84dfcdd387de6497067d745b248190efb0
VuHongViet <vuhongviet15498@gmail.com>
2019-11-07 18:46:35 +0700

    commit 07/11

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,42 +1,103 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
-import { Row, Col, Button, Input } from "antd";
+import {
+  Row,
+  Col,
+  Button,
+  Input,
+  Avatar,
+  Icon,
+  Badge,
+  Menu,
+  Dropdown
+} from "antd";
+import logo from "./../image/logo.png";
 export class HeaderComponent extends Component {
+  DKM = () => {
+    console.log("iframe loaded");
+    window.addEventListener("message", function(e) {
+      console.log(e.data);
+    });
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
   render() {
     const { Search } = Input;
 
-    //  nếu trong component có nút nào cần dispatch thì ta gọi nó
-    // như là 1 props this.props.onClickAdd();
-    return (
-      <Row
-        style={{
-          position: "relative"
-        }}
-      >
-        <Row
-          style={{
-            position: "fixed",
-            top: 0,
-            background: "#24292e",
-            width: "100%",
-            zIndex: "10",
-            padding: "10px 0",
-            height: "53px"
-          }}
-          type="flex"
-          gutter={16}
-          justify="end"
-        >
-          <Col span={2} order={2}>
+    const isAuthenticated = () => {
+      return (
+        <Col span={4} order={2}>
+          <Col span={12}>
             <Button>Đăng Nhập</Button>
           </Col>
-          <Col span={2} order={3}>
+          <Col span={12}>
             <Button onClick={this.LoadGetData}>Đăng Kí</Button>
           </Col>
-          <Col span={8} order={1}>
-            <Search enterButton placeholder="Tìm Kiếm" />
-          </Col>
-        </Row>
+        </Col>
+      );
+    };
+    const unAuthenticated = () => {
+      return (
+        <Col span={4}>
+          <Row type="flex" justify="center" align="middle">
+            <Col span={8} style={{ textAlign: "end", fontSize: 20 }}>
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            </Col>
+            <Col span={8} style={{ textAlign: "end", fontSize: 20 }}>
+              <Badge count={25} style={{ padding: 0 }}>
+                <Icon
+                  style={{
+                    borderRadius: "50%",
+                    background: "#ebedf0",
+                    padding: 5
+                  }}
+                  type="bell"
+                />
+              </Badge>
+            </Col>
+            <Col span={8} style={{ textAlign: "end", fontSize: 20 }}>
+              <Icon
+                style={{
+                  borderRadius: "50%",
+                  background: "#ebedf0",
+                  padding: 5
+                }}
+                type="question-circle"
+              />
+            </Col>
+          </Row>
+        </Col>
+      );
+    };
+    return (
+      <Row
+        type="flex"
+        justify="center"
+        style={{ background: "#24292e", padding: "10px 0" }}
+      >
+        <Col span={14}>
+          <Row type="flex" justify="center" align="middle">
+            <Col span={4}>
+              <img src={logo} />
+            </Col>
+            <Col span={16}>
+              <Search enterButton placeholder="Tìm Kiếm" />
+            </Col>
+            {this.state.isAuthenticated ? isAuthenticated() : unAuthenticated()}
+            <iframe
+              style={{ display: "none" }}
+              onLoad={this.DKM}
+              src="a.html"
+              width={0}
+              height={0}
+            ></iframe>
+          </Row>
+        </Col>
       </Row>
     );
   }
